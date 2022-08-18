29db76a4e5cbbbbffdb476869d08030e3027bcfd
gavinmorrow <gavinfmorrow@gmail.com>
2022-08-18 19:06:14 -0400

    Create GMMatrixError.swift

---
+++ Sources/GMMatrix/GMMatrixError.swift
@@ -0,0 +1,13 @@
+//
+//  GMMatrixError.swift
+//  
+//
+//  Created by Gavin Morrow on 8/18/22.
+//
+
+import Foundation
+
+public enum GMMatrixError: Error {
+	case xValueOutOfBounds
+	case yValueOutOfBounds
+}
